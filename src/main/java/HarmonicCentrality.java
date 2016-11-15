9e2c7314d68c824fd043a2bff6bf9cb0f74c6cf6
eugenio <eugenio.93@hotmail.it>
2016-11-15 14:43:29 +0100

    Completed implementation of the Eppstein algorithm.

--- src/main/java/HarmonicCentrality.java
+++ src/main/java/HarmonicCentrality.java
@@ -1,37 +1,48 @@
-/**
- * Created by Eugenio on 11/4/16.
- */
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
 
-import com.martiansoftware.jsap.*;
-import it.unimi.dsi.fastutil.ints.IntArrayList;
+import com.martiansoftware.jsap.FlaggedOption;
+import com.martiansoftware.jsap.JSAP;
+import com.martiansoftware.jsap.JSAPException;
+import com.martiansoftware.jsap.JSAPResult;
+import com.martiansoftware.jsap.Parameter;
+import com.martiansoftware.jsap.SimpleJSAP;
+import com.martiansoftware.jsap.Switch;
+import com.martiansoftware.jsap.UnflaggedOption;
+import it.unimi.dsi.fastutil.ints.IntArrayFIFOQueue;
 import it.unimi.dsi.fastutil.io.BinIO;
 import it.unimi.dsi.logging.ProgressLogger;
 import it.unimi.dsi.webgraph.ArrayListMutableGraph;
 import it.unimi.dsi.webgraph.ImmutableGraph;
 import it.unimi.dsi.webgraph.LazyIntIterator;
-import it.unimi.dsi.webgraph.examples.BreadthFirst;
-import org.apache.commons.lang.ArrayUtils;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorCompletionService;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.apache.commons.lang3.ArrayUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.*;
-import java.lang.reflect.Array;
-import java.util.Arrays;
-import java.util.Random;
-import java.util.concurrent.*;
-import java.util.concurrent.atomic.AtomicInteger;
-
-
 public class HarmonicCentrality {
     private static final Logger LOGGER = LoggerFactory.getLogger(HarmonicCentrality.class);
+    public static final double DEFAULT_ALPHA = 0.5D;
     private final ImmutableGraph graph;
+    public final double[] harmonic;
     private final ProgressLogger pl;
     private final int numberOfThreads;
     protected final AtomicInteger nextNode;
     protected volatile boolean stop;
-    public final double[] harmonic;
     private static final double precision = 0.2;
     private final int[] randomSamples;
+    private final double normalization;
 
     public HarmonicCentrality(ImmutableGraph graph, int requestedThreads, ProgressLogger pl) {
         this.pl = pl;
@@ -40,6 +51,7 @@ public class HarmonicCentrality {
         this.nextNode = new AtomicInteger();
         this.numberOfThreads = requestedThreads != 0?requestedThreads:Runtime.getRuntime().availableProcessors();
         this.randomSamples = pickRandomSamples(numberOfSamples());
+        this.normalization = (double)graph.numNodes() / ((double)(randomSamples.length * (graph.numNodes() - 1)));
     }
 
     public HarmonicCentrality(ImmutableGraph graph, ProgressLogger pl) {
@@ -127,169 +139,92 @@ public class HarmonicCentrality {
         if(this.pl != null) {
             this.pl.done();
         }
-
     }
 
-    public static void main(String[] arg) throws IOException, InterruptedException, JSAPException {
-//        SimpleJSAP jsap = new SimpleJSAP(HarmonicCentrality.class.getName(), "Computes the betweenness centrality a graph using an implementation of Brandes\'s algorithm based on multiple parallel breadth-first visits.", new Parameter[]{new Switch("expand", 'e', "expand", "Expand the graph to increase speed (no compression)."), new Switch("mapped", 'm', "mapped", "Use loadMapped() to load the graph."), new FlaggedOption("threads", JSAP.INTSIZE_PARSER, "0", false, 'T', "threads", "The number of threads to be used. If 0, the number will be estimated automatically."), new UnflaggedOption("graphBasename", JSAP.STRING_PARSER, JSAP.NO_DEFAULT, true, false, "The basename of the graph."), new UnflaggedOption("rankFilename", JSAP.STRING_PARSER, JSAP.NO_DEFAULT, true, false, "The filename where the resulting rank (doubles in binary form) are stored.")});
-//        JSAPResult jsapResult = jsap.parse(arg);
-//        if(jsap.messagePrinted()) {
-//            System.exit(1);
-//        }
-//
-//        boolean mapped = jsapResult.getBoolean("mapped", false);
-        String graphBasename = "/home/eugenio/Downloads/Graphs/cnr2000/cnr-2000-hc";
+    public static void main(String[] arg) throws IOException, JSAPException, InterruptedException {
+        SimpleJSAP jsap = new SimpleJSAP(HarmonicCentrality.class.getName(), "Computes positive centralities of a graph using multiple parallel breadth-first visits.\n\nPlease note that to compute negative centralities on directed graphs (which is usually what you want) you have to compute positive centralities on the transpose.",
+                new Parameter[]{
+                        new Switch("expand", 'e', "expand", "Expand the graph to increase speed (no compression)."),
+                        new Switch("mapped", 'm', "mapped", "Use loadMapped() to load the graph."),
+                        new FlaggedOption("threads", JSAP.INTSIZE_PARSER, "0", false, 'T', "threads", "The number of threads to be used. If 0, the number will be estimated automatically."),
+                        new UnflaggedOption("graphBasename", JSAP.STRING_PARSER, JSAP.NO_DEFAULT, true, false, "The basename of the graph."),
+                        new UnflaggedOption("harmonicFilename", JSAP.STRING_PARSER, JSAP.NO_DEFAULT, true, false, "The filename where harmonic centrality scores (doubles in binary form) will be stored.")
+        });
+        JSAPResult jsapResult = jsap.parse(arg);
+        if(jsap.messagePrinted()) {
+            System.exit(1);
+        }
+
+        boolean mapped = jsapResult.getBoolean("mapped", false);
+        String graphBasename = jsapResult.getString("graphBasename");
+        int threads = jsapResult.getInt("threads");
         ProgressLogger progressLogger = new ProgressLogger(LOGGER, "nodes");
         progressLogger.displayFreeMemory = true;
         progressLogger.displayLocalSpeed = true;
-        ImmutableGraph graph = ImmutableGraph.load(graphBasename, progressLogger); // mapped?ImmutableGraph.loadMapped(graphBasename, progressLogger):ImmutableGraph.load(graphBasename, progressLogger);
-//        if(jsapResult.userSpecified("expand")) {
-//            graph = (new ArrayListMutableGraph(graph)).immutableView();
-//        }
+        ImmutableGraph graph = mapped?ImmutableGraph.loadMapped(graphBasename, progressLogger):ImmutableGraph.load(graphBasename, progressLogger);
+        if(jsapResult.userSpecified("expand")) {
+            graph = (new ArrayListMutableGraph(graph)).immutableView();
+        }
 
-        HarmonicCentrality harmonicCentrality = new HarmonicCentrality(graph, 4, progressLogger);
-        harmonicCentrality.compute();
-        //BinIO.storeDoubles(harmonicCentrality.betweenness, rankFilename);
+        HarmonicCentrality centralities = new HarmonicCentrality(graph, threads, progressLogger);
+        centralities.compute();
+        for (double h : centralities.harmonic) {
+            System.out.println(h);
+        }
+        BinIO.storeDoubles(centralities.harmonic, jsapResult.getString("harmonicFilename"));
     }
 
     private final class IterationThread implements Callable<Void> {
-        private final IntArrayList queue;
-        private final IntArrayList cutPoints;
+        private final IntArrayFIFOQueue queue;
         private final int[] distance;
-        private final long[] sigma;
 
         private IterationThread() {
             this.distance = new int[HarmonicCentrality.this.graph.numNodes()];
-            this.sigma = new long[HarmonicCentrality.this.graph.numNodes()];
-            this.queue = new IntArrayList(HarmonicCentrality.this.randomSamples);
-            this.cutPoints = new IntArrayList();
-        }
-
-        private boolean checkOverflow(long[] sigma, int node, long currSigma, int s) {
-            if(sigma[s] > 9223372036854775807L - currSigma) {
-                throw new HarmonicCentrality.PathCountOverflowException(sigma[s] + " > " + (9223372036854775807L - currSigma) + " (" + node + " -> " + s + ")");
-            } else {
-                return true;
-            }
+            this.queue = new IntArrayFIFOQueue();
         }
 
         public Void call() {
             int[] distance = this.distance;
-            long[] sigma = this.sigma;
-            IntArrayList queue = this.queue;
+            IntArrayFIFOQueue queue = this.queue;
             ImmutableGraph graph = HarmonicCentrality.this.graph.copy();
+
 
             while(true) {
                 int curr = HarmonicCentrality.this.nextNode.getAndIncrement();
-                if (HarmonicCentrality.this.stop || curr >= randomSamples.length) {
+                if(HarmonicCentrality.this.stop || curr >= randomSamples.length) {
                     return null;
                 }
 
                 queue.clear();
-                queue.add(randomSamples[curr]);
-                LazyIntIterator succ;
-                this.cutPoints.clear();
-                this.cutPoints.add(0);
+                queue.enqueue(randomSamples[curr]);
                 Arrays.fill(distance, -1);
-                Arrays.fill(sigma, 0L);
                 distance[randomSamples[curr]] = 0;
-                sigma[randomSamples[curr]] = 1L;
-                boolean overflow = false;
-                int d;
-                int start;
-                int end;
-                int pos;
-                int pos1;
-                int s;
 
-                for(d = 0; queue.size() != this.cutPoints.getInt(this.cutPoints.size() - 1); ++d) {
-                    this.cutPoints.add(queue.size());
-                    start = this.cutPoints.getInt(d);
-                    end = this.cutPoints.getInt(d + 1);
+                while(!queue.isEmpty()) {
+                    int node = queue.dequeueInt();
+                    int d = distance[node] + 1;
+                    LazyIntIterator successors = graph.successors(node);
 
-                    for(pos = start; pos < end; ++pos) {
-                        pos1 = queue.getInt(pos);
-                        long node = sigma[pos1];
-                        try {
-                            succ = graph.successors(curr);
-                            while ((s = succ.nextInt()) != -1) {
-                                if (distance[s] == -1) {
-                                    distance[s] = d + 1;
-                                    queue.add(s);
-
-                                    assert this.checkOverflow(sigma, pos1, node, s);
-
-                                    overflow |= sigma[s] > 9223372036854775807L - node;
-                                    sigma[s] += node;
-                                } else if(distance[s] == d + 1) {
-                                    assert this.checkOverflow(sigma, pos1, node, s);
-
-                                    overflow |= sigma[s] > 9223372036854775807L - node;
-                                    sigma[s] += node;
-                                }
-                            }
-                        }
-                        catch (StackOverflowError e) {
-                            e.printStackTrace();
-                            System.out.println("Error");
+                    int s;
+                    while((s = successors.nextInt()) != -1) {
+                        if(distance[s] == -1) {
+                            queue.enqueue(s);
+                            distance[s] = d;
                         }
                     }
                 }
 
-                if(overflow) {
-                    throw new HarmonicCentrality.PathCountOverflowException();
+                int i = 0;
+                for (int d : distance) {
+                    HarmonicCentrality.this.harmonic[i++] += HarmonicCentrality.this.normalization * ((d > 0) ? 1.0D / (double)d : 0);
                 }
 
-                while (true) {
-                    --d;
-                    if (d <= 0) {
-                        if(HarmonicCentrality.this.pl != null) {
-                            synchronized(HarmonicCentrality.this.pl) {
-                                HarmonicCentrality.this.pl.update();
-                            }
-                        }
-                        break;
-                    }
-
-                    start = this.cutPoints.getInt(d);
-                    end = this.cutPoints.getInt(d + 1);
-
-                    for(pos = start; pos < end; ++pos) {
-                        pos1 = queue.getInt(pos);
-                        double var22 = (double)sigma[pos1];
-                        succ = graph.successors(pos1);
-
-                        while((s = succ.nextInt()) != -1) {
-                            if(distance[s] == d + 1) {
-                               // delta[pos1] += (1.0D + delta[s]) * var22 / (double)sigma[s];
-                            }
-                        }
-                    }
-
-                    HarmonicCentrality var21 = HarmonicCentrality.this;
-                    synchronized(HarmonicCentrality.this) {
-                        for(pos1 = start; pos1 < end; ++pos1) {
-                            int var23 = queue.getInt(pos1);
-                            //HarmonicCentrality.this.harmonic[var23] += delta[var23];
-                        }
+                if(HarmonicCentrality.this.pl != null) {
+                    synchronized(HarmonicCentrality.this.pl) {
+                        HarmonicCentrality.this.pl.update();
                     }
                 }
-
             }
         }
-
     }
-
-    public static final class PathCountOverflowException extends RuntimeException {
-        private static final long serialVersionUID = 1L;
-
-        public PathCountOverflowException() {
-        }
-
-        public PathCountOverflowException(String s) {
-            super(s);
-        }
-    }
-
 }
-
